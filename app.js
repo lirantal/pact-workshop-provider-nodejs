79d9d5493ef04d1dafc07ba938e4347b947d1a63
Liran Tal <liran.tal@gmail.com>
2019-04-07 11:16:43 +0300

    feat: barebones starting point

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
 const usersRouter = require('./routes/users')
-const reviewsRouter = require('./routes/reviewsRouter')
-const statsRouter = require('./routes/statsRouter')
 
 const app = express()
 
@@ -15,8 +13,7 @@ app.use(cookieParser())
 
 app.use('/users', usersRouter)
 app.use('/reviews', reviewsRouter)
-app.use('/stats', statsRouter)
-app.get('/health', function(req, res) {
+app.get('/health', function (req, res) {
   return res.status(200).end()
 })
 
--- routes/statsRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const StatsController = require('../controllers/StatsController')
-
-router.get('/', StatsController.get)
-
-module.exports = router
